656e6c2f474379a48baaf8607920399e8f72d970
Alan Galvão Martini <gmartinialan@gmail.com>
2023-07-22 12:10:19 -0400

    first implementation of Captcha.show injection

--- src/gameBot/GameBot.client.ts
+++ src/gameBot/GameBot.client.ts
@@ -3,6 +3,7 @@ import RpgMOSelectors from '../browserClient/enums/RpgMOSelectors.enum';
 import dotenv from 'dotenv';
 import PageHandler from './Handlers/Page.handler';
 import LoginHandler from './Handlers/Login.handler';
+import InjectionHandler from './Handlers/Injection.handler';
 
 dotenv.config();
 
@@ -10,20 +11,26 @@ class GameBot {
   private browserClient: BrowserClient;
   private loginHandler: LoginHandler;
   private pageHandler: PageHandler;
+  private injectionHandler: InjectionHandler;
 
   constructor(browserClient: BrowserClient) {
     this.browserClient = browserClient;
     this.pageHandler = new PageHandler(this.browserClient);
     this.loginHandler = new LoginHandler(this.browserClient, this.pageHandler);
+    this.injectionHandler = new InjectionHandler(this.browserClient);
   }
 
   async login() {
-    const selector = await this.browserClient.getPage()?.waitForSelector(RpgMOSelectors.WORLD_OPTIONS, {visible: true, timeout: 10000});
-    console.log('selector is:', selector);
+    await this.browserClient.getPage()?.waitForSelector(RpgMOSelectors.WORLD_OPTIONS, { visible: true, timeout: 10000 });
     
     await this.loginHandler.login();
+    await this.pageHandler.verifyIsLogged();
+
+    await this.injectionHandler.modifyCaptchaShow();
   }
 }
+
+
 
 export default GameBot;
 
